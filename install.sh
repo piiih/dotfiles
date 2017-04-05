2998cf1209ef3ec84cb81b6a9e2eb26cf3f0d8f5
piih~ <piih_@live.com>
2017-04-04 23:18:45 -0300

    Installer changed and tmux powerline directory

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ nvim_file=~/.config/nvim/init.vim
 if [ -f $tmux_file ]
 then
     mv ~/.tmux.conf ~/.tmux.conf.bkp
-    cp $script_dir/tmux/.tmux.conf ~/.tmux.conf
 fi
+cp $script_dir/tmux/.tmux.conf ~/.tmux.conf
 
 if [ -f $nvim_file ]
 then
     mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bkp
-    cp $script_dir/nvim/init.vim ~/.config/nvim/init.vim
 fi
+cp $script_dir/nvim/init.vim ~/.config/nvim/init.vim
